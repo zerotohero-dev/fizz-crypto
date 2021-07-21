8daec606a77c8701df2b90792c7fe6926764cabf
Volkan Ozcelik <me@volkan.io>
2021-07-21 16:18:04 -0700

    ALB Ingress path fixes

--- internal/api/api.go
+++ internal/api/api.go
@@ -22,56 +22,58 @@ import (
 	"github.com/zerotohero-dev/fizz-env/pkg/env"
 )
 
+const prefix = "/crypto"
+
 func InitializeEndpoints(e env.FizzEnv, router *mux.Router) {
 	svc := service.New(e, context.Background())
 
 	// Create a cryptographic hash.
-	app.Route(
-		router, http.NewServer(
+	app.RoutePrefixedPath(
+		http.NewServer(
 			endpoint.MakeHashCreateEndpoint(svc),
 			app.ContentTypeValidatingMiddleware(transport.DecodeHashCreateRequest),
 			app.EncodeResponse,
 		),
-		"POST", "/v1/hash",
+		router, "POST", prefix, "/v1/hash",
 	)
 
 	// Verify the hash.
-	app.Route(
-		router, http.NewServer(
+	app.RoutePrefixedPath(
+		http.NewServer(
 			endpoint.MakeHashVerifyEndpoint(svc),
 			app.ContentTypeValidatingMiddleware(transport.DecodeHashVerifyRequest),
 			app.EncodeResponse,
 		),
-		"POST", "/v1/hash/verify",
+		router, "POST", prefix, "/v1/hash/verify",
 	)
 
 	// Create a JSON Web Token.
-	app.Route(
-		router, http.NewServer(
+	app.RoutePrefixedPath(
+		http.NewServer(
 			endpoint.MakeJwtCreateEndpoint(svc),
 			app.ContentTypeValidatingMiddleware(transport.DecodeJwtCreateRequest),
 			app.EncodeResponse,
 		),
-		"POST", "/v1/jwt",
+		router, "POST", prefix, "/v1/jwt",
 	)
 
 	// Verify the JSON Web Token.
-	app.Route(
-		router, http.NewServer(
+	app.RoutePrefixedPath(
+		http.NewServer(
 			endpoint.MakeJwtVerifyEndpoint(svc),
 			app.ContentTypeValidatingMiddleware(transport.DecodeJwtVerifyRequest),
 			app.EncodeResponse,
 		),
-		"POST", "/v1/jwt/verify",
+		router, "POST", prefix, "/v1/jwt/verify",
 	)
 
 	// Create a random token.
-	app.Route(
-		router, http.NewServer(
+	app.RoutePrefixedPath(
+		http.NewServer(
 			endpoint.MakeTokenCreateEndpoint(svc),
 			app.ContentTypeValidatingMiddleware(transport.DecodeTokenCreateRequest),
 			app.EncodeResponse,
 		),
-		"GET", "/v1/token",
+		router, "GET", prefix, "/v1/token",
 	)
 }
