ffbd58153cfb857196a1aaa860b0d9c76e2fe16f
Volkan Ozcelik <me@volkan.io>
2021-11-01 16:16:27 -0700

    mTLS connectivity established.

--- cmd/main.go
+++ cmd/main.go
@@ -12,14 +12,45 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/spiffe/go-spiffe/v2/spiffetls"
+	"github.com/spiffe/go-spiffe/v2/spiffetls/tlsconfig"
 	"github.com/zerotohero-dev/fizz-app/pkg/app"
 	"github.com/zerotohero-dev/fizz-crypto/internal/api"
 	"github.com/zerotohero-dev/fizz-env/pkg/env"
 )
 
+/*
+
+TODO:
+x Install SPIRE server locally.
+* Launch crypto
+* Launch idm
+* register these two.
+* try establishing mTLS between them.
+/tmp/spire-server/private/api.sock
+ */
+
 func main() {
 	e := *env.New()
+
+	go func() {
+		fmt.Println("in go func")
+		// #region mTLS server
+		ctx := context.Background()
+		fmt.Println("creating the server…")
+		listener, err := spiffetls.Listen(ctx, "tcp", "127.0.0.1:8443", tlsconfig.AuthorizeAny())
+		fmt.Println(listener)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("Everything is awesome!")
+		}
+		fmt.Println("out of go func")
+		// #engregion mTLS server
+	}()
 
 	appEnv := e.Crypto
 	svcName := appEnv.ServiceName
--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/rs/cors v1.8.0 // indirect
 	github.com/shirou/gopsutil v3.21.7+incompatible // indirect
+	github.com/spiffe/go-spiffe/v2 v2.0.0-beta.10
 	github.com/zerotohero-dev/fizz-app v0.4.1
 	github.com/zerotohero-dev/fizz-entity v0.5.3
 	github.com/zerotohero-dev/fizz-env v0.3.7
